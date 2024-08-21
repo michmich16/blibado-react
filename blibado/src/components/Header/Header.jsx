b429e46f8661536b43b4af1a15c2601c89368f2f
Michael P <102651473+michmich16@users.noreply.github.com>
2024-08-21 10:12:19 +0200

    push

---
+++ blibado/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import style from './Header.module.scss'
+
+export const Header = () =>{
+
+return(
+    <header className={style.headerStyle}>
+        <h1>blibado</h1>
+        <h2>Vi ved hvor du bor!</h2>
+    </header>
+)
+
+}
